3ae7a6625c6cfafc9daf16ef2833b4f422f19cb3
driftbluestone <driftbluestone@gmail.com>
2026-05-25 15:24:36 -0700

    Update init.py

--- init.py
+++ init.py
@@ -3,7 +3,7 @@ from api import permission, config
 from pathlib import Path
 DIR = Path(__file__).resolve().parent
 
-permission.create("log_admin", "administrator")
+permission.create("log_admin", "Log Admin", "administrator")
 with open(f"{DIR}/actions.json", "r") as file:
     unlogged_actions = json.load(file)
 config.create_field(["logs", "logged_actions", "unlogged_actions"], [dict, dict, unlogged_actions])
