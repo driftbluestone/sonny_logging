5be09db76ccfe21f8044f6db1852cbea672fa8f8
driftbluestone <driftbluestone@gmail.com>
2026-06-13 22:56:02 -0700

    update to use jsonIO

--- init.py
+++ init.py
@@ -1,11 +1,11 @@
 import json
 from api import permission, config
+from utils import jsonIO
 from pathlib import Path
 DIR = Path(__file__).resolve().parent
 
 permission.create("log_admin", "Log Admin")
-with open(f"{DIR}/actions.json", "r") as file:
-    unlogged_actions = json.load(file)
+unlogged_actions = jsonIO.load(f"{DIR}/actions.json")
 
 config.create_field("logs", dict)
 config.create_field("logged_actions", dict)
